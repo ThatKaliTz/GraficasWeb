5ea0f693a7975a51a23bbc3833fbe5c46e7a10f0
૮ ˶ᵔ ᵕ ᵔ˶ ა <101676768+redbloom@users.noreply.github.com>
2024-03-08 15:51:26 -0600

    aaaaaaaaaaaaaaaaaaaaa

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        namecat: ["Namecat", "sans-serif"],
+      },
+      backgroundImage: {
+        'bg-texture': "url('/src/img/fondo.jpg')",
+        '1': "url('/src/img/bg1.jpg')",
+        '2': "url('/src/img/bg2.jpg')",
+        '3': "url('/src/img/bg3.png')",
+      }
+    },
   },
-  plugins: [],
+  plugins: [
+    require('tailwindcss-animated'),
+  ],
   darkMode:'class',
 }
